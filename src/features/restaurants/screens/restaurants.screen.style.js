dad8d9840eba1bc58dea8b031868a0e83f959136
woodylucas <wlucas0311@gmail.com>
2024-03-26 14:49:18 -0400

    feat: installed bottom tabs

--- src/features/restaurants/screens/restaurants.screen.style.js
+++ src/features/restaurants/screens/restaurants.screen.style.js
@@ -1,1 +1,15 @@
 import styled from "styled-components";
+
+import { FlatList } from "react-native";
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
+
+export { SearchContainer, RestaurantList };
