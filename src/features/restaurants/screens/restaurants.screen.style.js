e7568103011e5b417e98f8d914f209e765b1516a
woodylucas <wlucas0311@gmail.com>
2024-03-27 15:02:59 -0400

    feat: integrated an loading component from react-native-paper

--- src/features/restaurants/screens/restaurants.screen.style.js
+++ src/features/restaurants/screens/restaurants.screen.style.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 import { FlatList } from "react-native";
+import { ActivityIndicator } from "react-native-paper";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
@@ -12,4 +13,14 @@ const RestaurantList = styled(FlatList).attrs({
   },
 })``;
 
-export { SearchContainer, RestaurantList };
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
+export { SearchContainer, RestaurantList, Loading, LoadingContainer };
